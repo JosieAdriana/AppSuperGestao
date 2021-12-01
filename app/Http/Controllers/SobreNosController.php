ad6495d15c2638a3fe25cc6f1e26badec9084e8e
Josie Flôres <drijosie@gmail.com>
2021-12-01 14:20:46 -0300

    Adicionar data através de variável

--- app/Http/Controllers/SobreNosController.php
+++ app/Http/Controllers/SobreNosController.php
@@ -6,7 +6,11 @@ use Illuminate\Http\Request;
 
 class SobreNosController extends Controller
 {
-    public function SobreNos() {
-        return view('site.sobre-nos');
+    public function SobreNos()
+    {
+        return view('site.sobre-nos', [
+            "mensagem" => "Mensagem enviado por parâmetro através do Controlador",
+            "data" => date("D/M/Y H:i:s")
+        ]);
     }
 }
--- resources/views/site/sobre-nos.blade.php
+++ resources/views/site/sobre-nos.blade.php
@@ -1,1 +1,5 @@
 <h3>Sobre Nós(view)</h3>
+
+<p>Dia de hoje: {{ $data }}</p>
+
+<p>{{ $mensagem }}<p>
