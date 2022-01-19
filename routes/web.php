25f622ceeba77973f61b05aeb0947e9e0d890b50
Josie Flôres <drijosie@gmail.com>
2022-01-19 15:36:54 -0300

    Adicionando middlewares a um grupo de rotas

--- routes/web.php
+++ routes/web.php
@@ -24,34 +24,33 @@ Route::get("/", function () {
   return view('welcome');
 });
 
-Route::get('/principal', [PrincipalController::class , 'principal'])->name('site.index')->middleware('log.acesso');
+Route::get('/principal', [PrincipalController::class, 'principal'])->name('site.index')->middleware('log.acesso');
 
-Route::get('/sobre-nos', [SobreNosController::class , 'SobreNos'])->name('site.sobrenos');
+Route::get('/sobre-nos', [SobreNosController::class, 'SobreNos'])->name('site.sobrenos');
 // Route::get('/contato', [ContatoController::class , 'formularioContato'])->name('site.contato');
 
-Route::get('/contato', [ContatoController::class , 'contato'])->name('site.contato');
+Route::get('/contato', [ContatoController::class, 'contato'])->name('site.contato');
 
-Route::post('/contato', [ContatoController::class , 'salvar'])->name('site.contato');
+Route::post('/contato', [ContatoController::class, 'salvar'])->name('site.contato');
 Route::get('/login', function () {
   return 'Login';
 })->name('site.login');
 
-Route::prefix('app')->group(function () {
-  Route::middleware('autenticacao')
-  -> get('/clientes', function () {return 'Clientes';})
-  ->name('app.clientes');
+Route::middleware('autenticacao')->prefix('app')->group(function () {
+  Route::get('/clientes', function () {
+    return 'Clientes';
+  })->name('app.clientes');
 
-    /* PessoaController@telefones === [PessoaController::class, 'telefones'] */
+  /* PessoaController@telefones === [PessoaController::class, 'telefones'] */
 
-    Route::middleware('autenticacao')
-    ->get('fornecedores', [FornecedorController::class , 'index'])
-    ->name('app.fornecedores');
+  Route::get('fornecedores', [FornecedorController::class, 'index'])->name('app.fornecedores');
 
-    Route::middleware('autenticacao')
-    ->get('/produtos', function () {return 'Produtos';})
-    ->name('app.produtos');});
+  Route::get('/produtos', function () {
+    return 'Produtos';
+  })->name('app.produtos');
+});
 
-Route::get('/teste/{p1}/{p2}', [TesteController::class , 'teste'])->name('teste');
+Route::get('/teste/{p1}/{p2}', [TesteController::class, 'teste'])->name('teste');
 
 Route::fallback(function () {
   echo 'A rota acessada não existe, <a href= "' . route('site.index') . '"> clique aqui</a>  para ir para a página inicial.';
