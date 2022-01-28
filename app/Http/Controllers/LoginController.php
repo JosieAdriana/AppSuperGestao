c5d62dcd2608cddd2d7922c2a9eeeb6d6e192ac5
Josie Flôres <drijosie@gmail.com>
2022-01-28 13:26:32 -0300

    Inic a Sglobal Session e valid. aces. a rot proteg

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -15,6 +15,10 @@ class LoginController extends Controller
            $erro = 'Usuário e ou senha não existe';
        };
         
+       if($request->get('erro') ==2){
+        $erro = 'Necessário efetuar login para ter acesso a página';
+    };
+       
         return view('site.login', ['titulo' => 'Login', 'erro'=> $erro]);
     }
 
@@ -39,8 +43,7 @@ class LoginController extends Controller
         $email = $request->get('usuario');
         $password = $request->get('senha');
 
-        echo "Usuário: $email | Senha: $password";
-        echo '<br>';
+       
 
         //iniciar o Model User
         $user = new User();
@@ -50,7 +53,13 @@ class LoginController extends Controller
             ->get()->first();
 
         if(isset($usuario->name)){
-            echo 'Usuário existe';
+          
+            session_start();
+            $_SESSION['nome'] = $usuario->name;
+            $_SESSION['email'] = $usuario->email;
+            
+            return redirect()->route('app.clientes');
+
         } else {
             return redirect()->route('site.login', ['erro'=> 1]);
         }
