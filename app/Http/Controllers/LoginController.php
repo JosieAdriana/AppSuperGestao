238cbafc3f3ae8002ab2adffd4b90c89e28be676
Josie Flôres <drijosie@gmail.com>
2022-01-28 11:01:41 -0300

    Validando a existência do usuário e senha no Banco

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,21 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class LoginController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('site.login', ['titulo' => 'Login']);
     }
 
-    public function autenticar(Request $request){
-       
+    public function autenticar(Request $request)
+    {
+
         //regras de validação
         $regras = [
             'usuario' => 'email',
             'senha' => 'required'
         ];
- 
+
         //as mensagens de feedback de validação
         $feedback = [
             'usuario.email' => 'O campo usuário (e-mail) é obrigatório',
@@ -26,6 +29,25 @@ class LoginController extends Controller
 
         $request->validate($regras, $feedback);
 
-        print_r($request->all());
+        //recuperamos os parametros do formulario
+        $email = $request->get('usuario');
+        $password = $request->get('senha');
+
+        echo "Usuário: $email | Senha: $password";
+        echo '<br>';
+
+        //iniciar o Model User
+        $user = new User();
+
+        $usuario = $user->where('email', $email)
+            ->where('password', $password)
+            ->get()->first();
+
+        if(isset($usuario->name)){
+            echo 'Usuário existe';
+        } else {
+            echo 'Usuário não existe';
+        }
+       
     }
 }
