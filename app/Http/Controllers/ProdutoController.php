7d005308bc3fb4f1426637203c2c986b2e31a54c
Josie Flôres <drijosie@gmail.com>
2022-02-18 21:32:38 -0300

    Eloquent ORM 1 para N - Associando fornecedores a produtos (Store e Update)

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -6,6 +6,7 @@ use App\Models\Produto;
 use App\Models\Item;
 use App\Models\ProdutoDetalhe;
 use App\Models\Unidade;
+use App\Models\Fornecedor;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
 
@@ -48,7 +49,8 @@ class ProdutoController extends Controller
     public function create()
     {
         $unidades = Unidade::all();
-        return view('app.produto.create', ['unidades' => $unidades]);
+        $fornecedores = Fornecedor::all();
+        return view('app.produto.create', ['unidades' => $unidades, 'fornecedores' => $fornecedores]);
     }
 
     /**
@@ -64,6 +66,7 @@ class ProdutoController extends Controller
         'descricao' => 'required|min:3|max:2000',
         'peso' => 'required|integer',
         'unidade_id' => 'exists:unidades,id',
+        'fornecedor_id' => 'exists:fornecedores,id'
        ];
        
        $feedback = [
@@ -73,13 +76,13 @@ class ProdutoController extends Controller
         'descricao.min' => 'O campo descrição deve ter no mínimo 3 caracteres',
         'descricao.max' => 'O campo descrição deve ter no máximo 40 caracteres',
         'peso.integer' => 'O campo peso deve ser um número inteiro',
-        'unidade_id.exists' => 'A unidade de medida informada não existe'
-        
+        'unidade_id.exists' => 'A unidade de medida informada não existe',
+        'fornecedor_id.exists' => 'O fornecedor informado não existe'
        ];
 
        $request->validate($regras, $feedback);
        
-        Produto::create($request->all());
+        Item::create($request->all());
         return redirect()->route('produto.index');
     }
 
@@ -103,7 +106,8 @@ class ProdutoController extends Controller
     public function edit(Produto $produto)
     {   
         $unidades = Unidade::all();
-        //return view( 'app.produto.edit', ['produto' => $produto, 'unidades' => $unidades]);
+        $fornecedores = Fornecedor::all();
+        return view( 'app.produto.edit', ['produto' => $produto, 'unidades' => $unidades, 'fornecedores' => $fornecedores]);
         //return view( 'app.produto.create', ['produto' => $produto, 'unidades' => $unidades]);
     }
 
@@ -111,15 +115,38 @@ class ProdutoController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Produto  $produto
+     * @param  \App\Models\Item  $produto
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Produto $produto)
+    public function update(Request $request, Item $produto)
     {
+        $regras = [
+            'nome' => 'required|min:3|max:40',
+            'descricao' => 'required|min:3|max:2000',
+            'peso' => 'required|integer',
+            'unidade_id' => 'exists:unidades,id',
+            'fornecedor_id' => 'exists:fornecedores,id',
+           ];
+           
+           $feedback = [
+            'required' => 'O campo nome deve ser preenchido',
+            'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+            'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+            'descricao.min' => 'O campo descrição deve ter no mínimo 3 caracteres',
+            'descricao.max' => 'O campo descrição deve ter no máximo 40 caracteres',
+            'peso.integer' => 'O campo peso deve ser um número inteiro',
+            'unidade_id.exists' => 'A unidade de medida informada não existe',
+            'fornecedor_id.exists' => 'O fornecedor informado não existe'
+            
+           ];
+    
+           $request->validate($regras, $feedback);
+
         print_r($request->all()); //payload
         echo '<br><br><br>';
         print_r($produto->getAttributes()); //instancia do objeto no estado anterior
 
+       // dd($request->all());
         $produto->update($request->all());
         return redirect()->route('produto.show', ['produto' => $produto->id]);
     }
--- app/Models/Item.php
+++ app/Models/Item.php
@@ -9,7 +9,7 @@ class Item extends Model
 {   
     protected $table = 'produtos';
     use HasFactory;
-    protected $fillable = ['nome', 'descricao', 'peso', 'unidade_id' ];
+    protected $fillable = ['nome', 'descricao', 'peso', 'unidade_id', 'fornecedor_id'];
 
     public function itemDetalhe(){
         return $this->hasOne('App\Models\ItemDetalhe', 'produto_id', 'id' );
