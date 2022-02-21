a01c2e2d38d33958a1f61e856bb0cc9f7f6632be
Josie Flôres <drijosie@gmail.com>
2022-02-21 09:53:00 -0300

    Criando os controladores e rotas para clientes, pedidos e pedidos produtos

--- routes/web.php
+++ routes/web.php
@@ -37,7 +37,6 @@ Route::post('/login', [LoginController::class, 'autenticar'])->name('site.login'
 Route::middleware('autenticacao:padrao, visitante')->prefix('app')->group(function () {
   Route::get('/home', [HomeController::class, 'index'])->name('app.home');
   Route::get('/sair', [LoginController::class, 'sair'])->name('app.sair');
-  Route::get('/cliente', [ClienteController::class, 'index'])->name('app.cliente');
   Route::get('/fornecedor', [FornecedorController::class, 'index'])->name('app.fornecedor');
   Route::post('/fornecedor/listar', [FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
   Route::get('/fornecedor/listar', [FornecedorController::class, 'listar'])->name('app.fornecedor.listar');
@@ -51,6 +50,10 @@ Route::middleware('autenticacao:padrao, visitante')->prefix('app')->group(functi
 
   //produtos detalhes
   Route::resource('produto-detalhe',  ProdutoDetalheController::class);
+
+  Route::resource('cliente', 'ClienteController');
+  Route::resource('pedido', 'PedidoController');
+  Route::resource('pedido_produto', 'PedidoProdutoController');
 });
 
 
