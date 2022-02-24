1a4844eaf73bc92f6bef3b67ad30293794df2aaf
Josie Flôres <drijosie@gmail.com>
2022-02-24 18:50:30 -0300

    Relacionamento N para N - Removendo o relacionamento

--- app/Http/Controllers/PedidoProdutoController.php
+++ app/Http/Controllers/PedidoProdutoController.php
@@ -115,8 +115,28 @@ class PedidoProdutoController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Pedido $pedido, Produto $produto)
     {
-        //
+        /*
+        print_r($pedido->getAttributes());
+        echo 'hr';
+        print_r($produto->getAttributes());
+        */
+
+        echo $pedido->id.'-'.$produto->id;
+
+        //convencional
+        /*
+        PedidoProduto::where([
+            'pedido_id' => $pedido->id,
+            'produto_id' => $produto->id
+        ])->delete();
+        */
+
+        //detach (delete pelo relacionamento)
+        $pedido->produtos()->detach($produto->id);
+        //pedido_id
+
+        return redirect()->route('pedido-produto.create', ['pedido' => $pedido->id]);
     }
 }
--- resources/views/app/pedido_produto/create.blade.php
+++ resources/views/app/pedido_produto/create.blade.php
@@ -31,6 +31,7 @@
                             <th>ID</th>
                             <th>Nome do produto</th>
                             <th>Data de inclusão do item no pedido</th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -39,6 +40,13 @@
                             <td>{{ $produto->id }}</td>
                             <td>{{ $produto->nome }}</td>
                             <td>{{ $produto->pivot->created_at->format('d/m/y')}}</td>
+                            <td>
+                                <form id="form_{{$pedido->id}}_{{$produto->id}}" method="post" action="{{ route('pedido-produto.destroy', ['pedido' => $pedido->id, 'produto' => $produto->id ])}}">
+                                    @method('DELETE')
+                                    @csrf
+                                <a href="#" onclick="document.getElementById('form_{{$pedido->id}}_{{$produto->id}}').submit()" >Excluir</a>
+                                </form>
+                            </td>
                         </tr>
                         @endforeach
                     </tbody>
