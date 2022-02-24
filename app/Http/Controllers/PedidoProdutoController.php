71b8322bf223f711c1f1b9311305759758d7d8cc
Josie Flôres <drijosie@gmail.com>
2022-02-24 14:30:34 -0300

    Eloquent ORM N para N - Implementando o relacionamento belongsToMany

--- app/Http/Controllers/PedidoProdutoController.php
+++ app/Http/Controllers/PedidoProdutoController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Pedido;
 use App\Models\Produto;
+use App\Models\PedidoProduto;
 
 class PedidoProdutoController extends Controller
 {
@@ -26,6 +27,7 @@ class PedidoProdutoController extends Controller
     public function create(Pedido $pedido)
     {   
         $produtos = Produto::all();
+        //$pedido->produtos;  //eager loading
         return view('app.pedido_produto.create', ['pedido' => $pedido, 'produtos' => $produtos]);
     }
 
@@ -37,13 +39,25 @@ class PedidoProdutoController extends Controller
      */
     public function store(Request $request, Pedido $pedido)
     {
-        echo '<pre>';
-        print_r($pedido);
-        echo '<pre>';
-        echo '<hr>';
-        echo '<pre>';
-        print_r($request->all());
-        echo '<pre>';
+        //dd($request->all());
+
+        $regras = [
+            'produto_id' => 'exists:produtos,id'
+        ];
+
+        $feedback = [
+            'produto_id.exists' => 'O produto informado não existe.'
+        ];
+
+        $request->validate($regras, $feedback);
+
+       
+        $pedidoProduto = new PedidoProduto();
+        $pedidoProduto->pedido_id = $pedido->id;
+        $pedidoProduto->produto_id = $request->get('produto_id');
+        $pedidoProduto->save();
+
+        return redirect()->route('pedido-produto.create', ['pedido' => $pedido->id]);
     }
 
     /**
